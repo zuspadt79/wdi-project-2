ac93eae50ed3e15c17be7a579bd757b043e96875
zuspadt79 <samuel.hey@gmail.com>
2016-09-16 10:54:32 +0100

    added Procfile

--- ev-network/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/express-authentication-jwt',
-  secret: process.env.SECRET || "secret thing"
-
-};
--- ev-network/controllers/statics.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  home: staticsHome
-};
-
-const path = require("path");
-
-function staticsHome(req, res){
-  return res.sendFile(path.join(__dirname, "../index.html"));
-}
