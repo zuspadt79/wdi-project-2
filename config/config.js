d38264ab871e1b9627254e6cccc44c32a4ff260d
zuspadt79 <samuel.hey@gmail.com>
2016-09-16 10:57:13 +0100

    added mongolab to config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/express-authentication-jwt',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost/express-authentication-jwt',
   secret: process.env.SECRET || "secret thing"
 
 };
